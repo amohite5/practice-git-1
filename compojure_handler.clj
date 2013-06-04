90a21d01c1e4fc8220a067005c283e05227bf295
wslaurance <wslaurance@missionfocus.com>
2013-06-04 09:09:17 -0400

    Do the necessary clojure 3.0.3 switch to php syntax

--- compojure_handler.clj
+++ compojure_handler.clj
@@ -1,31 +1,31 @@
-(ns compojure.handler
+$ns compojure.handler
   "Functions to create Ring handlers from routes."
-  (:use [ring.middleware params
+  $:use [ring.middleware params
                          keyword-params
                          nested-params
                          multipart-params
                          cookies
                          session
-                         flash]))
+                         flash]$$
 
-(defn- with-opts [routes middleware opts]
-  (if opts
-    (middleware routes opts)
-    (middleware routes)))
+$defn- with-opts [routes middleware opts]
+  $if opts
+    $middleware routes opts$
+    $middleware routes$$$
 
-(defn api
+$defn api
   "Create a handler suitable for a web API. This adds the following
   middleware to your routes:
     - wrap-params
     - wrap-nested-params
     - wrap-keyword-params"
   [routes]
-  (-> routes
+  $-> routes
       wrap-keyword-params
       wrap-nested-params
-      wrap-params))
+      wrap-params$$
 
-(defn site
+$defn site
   "Create a handler suitable for a standard website. This adds the
   following middleware to your routes:
     - wrap-session
@@ -40,7 +40,7 @@
     :session   - a map of session middleware options
     :multipart - a map of multipart-params middleware options"
   [routes & [opts]]
-  (-> (api routes)
-      (with-opts wrap-multipart-params (:multipart opts))
-      (wrap-flash)
-      (with-opts wrap-session (:session opts))))
+  $-> $api routes$
+      $with-opts wrap-multipart-params $:multipart opts$$
+      $wrap-flash$
+      $with-opts wrap-session $:session opts$$$$
